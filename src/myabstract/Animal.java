ae67e6d382e4804ac07df5c57fdedb02e351801a
Eric <Eric@10.30.18.237>
2017-09-14 00:47:40 -0500

    Finished the abstract package. Made Animal superclass abstract and made subclasses extend Animal class

---
+++ src/myabstract/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myabstract;
+
+/**
+ *
+ * @author Eric
+ */
+public abstract class Animal {
+    private String phylum;
+
+    public String getPhylum() {
+        return phylum;
+    }
+
+    public void setPhylum(String phylum) {
+        this.phylum = phylum;
+    }
+}
---
+++ src/myabstract/Mammal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myabstract;
+
+/**
+ *
+ * @author Eric
+ */
+public class Mammal extends Animal {
+    private String clasS;
+
+    public String getClasS() {
+        return clasS;
+    }
+
+    public void setClasS(String clasS) {
+        this.clasS = clasS;
+    }
+}
