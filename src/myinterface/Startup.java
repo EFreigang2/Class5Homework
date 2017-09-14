d77803072e4a5bfc3cc1d3478c339381f978aac2
Eric <Eric@10.30.18.237>
2017-09-14 13:43:21 -0500

    Finished the interface package. Getting error saying identifier expected

--- src/myinterface/Startup.java
+++ src/myinterface/Startup.java
@@ -10,13 +10,13 @@ package myinterface;
  * @author Eric
  */
 public class Startup {
-    Cat cat = new Cat();
-    cat.setPhylum("Chordata");
-    cat.setClasS("Mammalia");
-    cat.setGenus("Felis");
-    cat.setSpecies("concolor");
-    cat.setName("Puma");
-    System.out.println("The following animal's taxanomical name is: " + 
-    cat.getPhylum() + " " + cat.getClasS() + " " + cat.getGenus() + " " + 
-    cat.getSpecies() + ". Otherwise known as a " + cat.getName());
+//    Cat cat = new Cat();
+//    cat.setPhylum("Chordata");
+//    cat.setClasS("Mammalia");
+//    cat.setGenus("Felis");
+//    cat.setSpecies("concolor");
+//    cat.setName("Puma");
+//    System.out.println("The following animal's taxanomical name is: " + 
+//    cat.getPhylum() + " " + cat.getClasS() + " " + cat.getGenus() + " " + 
+//    cat.getSpecies() + ". Otherwise known as a " + cat.getName());
 }
